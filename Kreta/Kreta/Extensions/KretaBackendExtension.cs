db959b9f7a8729e5e93819fe07621a4d2d19042d
Krisztin Benedek <krisztinbeni@gmail.com>
2024-11-22 10:33:50 +0100

    Cors

--- Kreta/Kreta/Extensions/KretaBackendExtension.cs
+++ Kreta/Kreta/Extensions/KretaBackendExtension.cs
@@ -4,12 +4,21 @@
     {
         public static void AddBackend(this IServiceCollection services)
         {
-
+            services.ConfigureCors();
         }
 
-        private static void ConfigureCors(IServiceCollection services)
+        private static void ConfigureCors(this IServiceCollection services)
         {
-
+            services.AddCors(option =>
+                 option.AddPolicy(name: "KretaCors",
+                     policy =>
+                     {
+                         policy.WithOrigins("https://0.0.0.0:7020/")
+                         .AllowAnyHeader()
+                         .AllowAnyMethod();
+                     }
+                 )
+            );
         }
     }
 }
