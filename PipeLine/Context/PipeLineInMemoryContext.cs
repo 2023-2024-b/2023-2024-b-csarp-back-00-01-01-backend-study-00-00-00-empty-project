5361893befdc98301f1f873a5e47873face2b7a5
b0gavmar <b0gavmar@vasvari.org>
2024-11-22 11:11:32 +0100

    ModelBuilderExtensions,PipeLineInMemoryContext, EntityFrameworkCore-t is használunk

---
+++ PipeLine/Context/PipeLineInMemoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PipeLine.Context
+{
+    public class PipeLineInMemoryContext: PipeLineContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Seed();
+        }
+    }
+}
