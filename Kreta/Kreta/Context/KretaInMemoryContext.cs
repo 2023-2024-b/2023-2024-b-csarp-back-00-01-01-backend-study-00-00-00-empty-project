cd3a68287197fb86d3834539a426526f335dea50
Krisztin Benedek <krisztinbeni@gmail.com>
2024-11-22 11:34:38 +0100

    KretaInMemoryContext problémák

--- Kreta/Kreta/Context/KretaInMemoryContext.cs
+++ Kreta/Kreta/Context/KretaInMemoryContext.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Kreta.Context
 {
     public class KretaInMemoryContext : KretaContext
     {
+        public KretaInMemoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
