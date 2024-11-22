15401d19bb8fbf10330cd1e4f92cdb750121aa7d
Krisztin Benedek <krisztinbeni@gmail.com>
2024-11-22 10:26:21 +0100

    ConfigureCors

---
+++ Kreta/Kreta/Extensions/KretaBackendExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Kreta.Extensions
+{
+    public static class KretaBackendExtension
+    {
+        public static void AddBackend(this IServiceCollection services)
+        {
+
+        }
+
+        private static void ConfigureCors(IServiceCollection services)
+        {
+
+        }
+    }
+}
