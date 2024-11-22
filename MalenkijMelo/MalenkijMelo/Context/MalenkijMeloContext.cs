feac998b5b21df3218ac37c43f8c289b1115515d
Toth-Mate <t0thm4t3@gmail.com>
2024-11-22 11:34:05 +0100

    Context - Konstruktor

--- MalenkijMelo/MalenkijMelo/Context/MalenkijMeloContext.cs
+++ MalenkijMelo/MalenkijMelo/Context/MalenkijMeloContext.cs
@@ -4,6 +4,9 @@ namespace MalenkijMelo.Context
 {
     public class MalenkijMeloContext : DbContext
     {
+        protected MalenkijMeloContext(DbContextOptions options) : base(options)
+        {
 
+        }
     }
 }
--- MalenkijMelo/MalenkijMelo/Context/MalenkijMeloInMemoryContext.cs
+++ MalenkijMelo/MalenkijMelo/Context/MalenkijMeloInMemoryContext.cs
@@ -4,6 +4,11 @@ namespace MalenkijMelo.Context
 {
     public class MalenkijMeloInMemoryContext : MalenkijMeloContext
     {
+        protected MalenkijMeloInMemoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
