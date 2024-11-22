5eb53dfe052784c2860b4377a0271c4f87e508f7
Krisztin Benedek <krisztinbeni@gmail.com>
2024-11-22 10:57:38 +0100

    ConfigureWebHost

--- Kreta/Kreta/Program.cs
+++ Kreta/Kreta/Program.cs
@@ -1,3 +1,5 @@
+using Kreta.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.ConfigureWebHost();
+builder.Services.AddBackend();
 
 var app = builder.Build();
 
@@ -22,4 +27,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors("KretaCors");
+
 app.Run();
+
