d17f30581bb2f97246dc119df45e3f647c47277c
b0totdav@vasvari.org <b0totdav@vasvari.org>
2024-11-22 10:33:52 +0100

    1.1

---
+++ MalenkijMelo.Api/MalenkijMelo.Api/Controllers/Extension/KreateBackendExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace MalenkijMelo.Api.Controllers.Extension
+{
+    public static class KreateBackendExtension
+    {
+        public static void AddBackend(this IServiceCollection services)
+        {
+            services.ConfigureCors();
+        }
+
+        private static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(option =>
+                option.AddPolicy(name: "KretaCors",
+                    policy =>
+                    {
+                        policy.WithOrigins("https://0.0.0.0:7020/")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    }
+                )
+           );
+        }
+    }
+}
