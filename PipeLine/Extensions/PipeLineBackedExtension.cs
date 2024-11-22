96bd84d6c2e4da22996e035fc22fd5091b542d43
b0gavmar <b0gavmar@vasvari.org>
2024-11-22 10:33:56 +0100

    AddBackend, ConfigureCors

---
+++ PipeLine/Extensions/PipeLineBackedExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace PipeLine.Extensions
+{
+    public static class PipeLineBackedExtension
+    {
+        public static void AddBackend(this IServiceCollection services)
+        {
+            services.ConfigureCors();
+        }
+
+        private static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(option =>
+                option.AddPolicy(name: "PipeLineCors",
+                    policy =>
+                    {
+                        policy.WithOrigins("https://0.0.0.0:7020/")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    }
+                )
+           );
+        }
+    }
+}
