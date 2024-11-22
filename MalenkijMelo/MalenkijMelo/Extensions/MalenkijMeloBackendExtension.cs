1ee0d544e9c61fae4a8d719b7c9d1a3e378223bb
Toth-Mate <t0thm4t3@gmail.com>
2024-11-22 10:26:03 +0100

    BackendExtension

---
+++ MalenkijMelo/MalenkijMelo/Extensions/MalenkijMeloBackendExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace MalenkijMelo.Extensions
+{
+    public static class MalenkijMeloBackendExtension
+    {
+        public static void AddBackend(this IServiceCollection services)
+        {
+
+        }
+
+        private static void ConfigureCors(IServiceCollection services)
+        {
+
+        }
+    }
+}
