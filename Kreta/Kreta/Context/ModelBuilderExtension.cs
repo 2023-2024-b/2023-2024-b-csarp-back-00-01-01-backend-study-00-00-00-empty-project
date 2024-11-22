9706900761219147518a763f6f9666a187061a4d
Krisztin Benedek <krisztinbeni@gmail.com>
2024-11-22 11:10:04 +0100

    KretaInMemoryContext

---
+++ Kreta/Kreta/Context/ModelBuilderExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace Kreta.Context
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            //ide kerülnek a teszt adatok
+        }
+    }
+}
